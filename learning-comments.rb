a1a5d54bed41f35a58ef16df4d78b3cb143a069b
Roberta Flores <r.flores.1227@gmail.com>
2019-09-09 04:39:13 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,15 @@
+# Perform a constant expression evaluation
+3
+# Assing constant 3 to bare-words variable triangle_sides
+'triangle_sides'
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+name = "Byron"
+# name = "Luca"
+
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
